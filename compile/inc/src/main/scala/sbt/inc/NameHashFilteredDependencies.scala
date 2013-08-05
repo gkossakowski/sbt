bc604235366fc6113d2dd3213018fa220b895867
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2013-08-05 15:50:38 -0700

    Cache results of name-hash-based filtering.

--- compile/inc/src/main/scala/sbt/inc/NameHashFilteredDependencies.scala
+++ compile/inc/src/main/scala/sbt/inc/NameHashFilteredDependencies.scala
@@ -10,25 +10,29 @@ class NameHashFilteredDependencies(
 		allModifiedNames: Map[File, Set[String]],
 		log: Logger) extends (File => Set[File]) {
 
+	private val cachedResults: collection.mutable.Map[File, Set[File]] = collection.mutable.Map.empty
+
 	def apply(to: File): Set[File] = {
 		val dependent = reversedMemberRefDeps(to)
-		allModifiedNames.get(to) match {
-			case None =>
-				log.debug("Information about modified names in %s was not available so all dependencies are considered: %s".format(to, dependent))
-				dependent
-			case Some(modifiedNames) =>
-				dependent.filter { from: java.io.File =>
-					val usedNamesInDependent = usedNames(from)
-					val modifiedAndUsedNames = modifiedNames intersect usedNamesInDependent
-					if (modifiedAndUsedNames.isEmpty) {
-						log.debug("Information about modified names in %s was available but none of the modified names appears in %s. This dependency is not being considered for invalidation.".format(to, from))
-						false
-					} else {
-						log.debug("The following modified names in %s cause invalidation of %s: %s".format(to, from, modifiedAndUsedNames))
-						true
-					}
+		cachedResults.getOrElseUpdate(to, filteredDependencies(to, dependent))
+	}
+
+	private def filteredDependencies(to: File, dependent: Set[File]) = allModifiedNames.get(to) match {
+		case None =>
+			log.debug("Information about modified names in %s was not available so all dependencies are considered: %s".format(to, dependent))
+			dependent
+		case Some(modifiedNames) =>
+			dependent.filter { from: java.io.File =>
+				val usedNamesInDependent = usedNames(from)
+				val modifiedAndUsedNames = modifiedNames intersect usedNamesInDependent
+				if (modifiedAndUsedNames.isEmpty) {
+					log.debug("Information about modified names in %s was available but none of the modified names appears in %s. This dependency is not being considered for invalidation.".format(to, from))
+					false
+				} else {
+					log.debug("The following modified names in %s cause invalidation of %s: %s".format(to, from, modifiedAndUsedNames))
+					true
 				}
-		}
+			}
 	}
 
 	private def usedNames(from: File): Set[String] = names.forward(from)

700c8be9493611e7a3ef7d9a360d37b89ba7f924
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2013-09-21 21:05:51 -0500

    Factor out common names in ExtractUsedNamesTest.
    That test would contain all expected names. Some of them are standard
    names that are always extracted. We are factoring it out to a separate
    variable so the distinction is a little bit more clear.

--- compile/interface/src/test/scala/xsbt/ExtractUsedNamesTest.scala
+++ compile/interface/src/test/scala/xsbt/ExtractUsedNamesTest.scala
@@ -12,6 +12,18 @@ import org.junit.Assert._
 @RunWith(classOf[JUnit4])
 class ExtractUsedNamesTest {
 
+	/**
+	 * Standard names that appear in every compilation unit that has any class
+	 * definition.
+	 */
+	private val standardNames = Set(
+		// AnyRef is added as default parent of a class
+		"scala", "AnyRef",
+		// class receives a default constructor which is internally called "<init>"
+		"<init>",
+		// AnyRef is dealiased into Object
+		"Object")
+
 	@Test
 	def importedName: Unit = {
 		val src = """
@@ -21,15 +33,9 @@ class ExtractUsedNamesTest {
 			|}""".stripMargin
 		val compilerForTesting = new ScalaCompilerForUnitTesting
 		val usedNames = compilerForTesting.extractUsedNamesFromSrc(src)
-		val expectedNames =
-			Set("a", "A", "A2",
-				// AnyRef is added as default parent of a class A
-				"scala", "AnyRef",
-				// class A receives default constructor which is internally called "<init>"
-				"<init>",
-				// coming from CompilationUnit.depends
-				"Object",
-				"b")
+		val expectedNames = standardNames ++ Set("a", "A", "A2", "b")
+		assertEquals(expectedNames, usedNames)
+	}
 		assertEquals(expectedNames, usedNames)
 	}
 

b1a6a173d30b56c1cbcc3c04e5dcdf71ffbf97ff
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2014-02-17 14:49:34 +0100

    WIP on implementing eviction policy for AnalysisStoreCache.

--- compile/integration/src/main/scala/sbt/compiler/AnalysisStoreCache.scala
+++ compile/integration/src/main/scala/sbt/compiler/AnalysisStoreCache.scala
@@ -9,8 +9,8 @@ import sbt.inc.{AnalysisStore, FileBasedStore}
  * Cache of AnalysisStore objects. Given the fact that it's implemented using
  * singleton object the cache is global within single classloader.
  *
- * NOTE: This cache comes with no explicit invalidation policy. The only way to invalidate
- * entries in this cache is to trigger GC that will collect SoftReferences.
+ * NOTE: Entries in this cache are evicted when last modified timestamp of
+ * `analysisFile` changes. This way the cache stays in sync with
  *
  * NOTE: There's also AnalysisStore.cached which caches the underlying Analysis object.
  * Here we cache those caching AnalysisStore instances.
@@ -21,12 +21,27 @@ private[sbt] object AnalysisStoreCache {
 		staticCache(analysisFile, AnalysisStore.sync(AnalysisStore.cached(FileBasedStore(analysisFile))))
 	}
 
-	private[this] val cache = new collection.mutable.HashMap[File, Reference[AnalysisStore]]
+	/**
+	 * Implements SoftReference that is cleared in two ways:
+	 *
+	 *   1. By GC with exactly the same semantics as regular SoftReference
+	 *   2. By touching (changing last modified timestamp) of the file that was passed as constructor
+	 *      argument `backingFile` to this reference.
+	 */
+	private class FileBackedSoftReference[T >: Null](backingFile: File, referent: T) extends SoftReference[T](referent) {
+		private val savedLastModified = backingFile.lastModified
+		override def get(): T = {
+			if (backingFile.lastModified != savedLastModified)
+				this.clear()
+			super.get
+		}
+	}
+	private[this] val analysisStoreCache = new collection.mutable.HashMap[File, Reference[AnalysisStore]]
 	private def staticCache(file: File, backing: => AnalysisStore): AnalysisStore =
 		synchronized {
-			cache get file flatMap { ref => Option(ref.get) } getOrElse {
+			analysisStoreCache get file flatMap { ref => Option(ref.get) } getOrElse {
 				val b = backing
-				cache.put(file, new SoftReference(b))
+				analysisStoreCache.put(file, new FileBackedSoftReference(file, b))
 				b
 			}
 		}
